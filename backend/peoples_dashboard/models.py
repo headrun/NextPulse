c11c7c1667874f894c8d794ed340675a68d62359
Abhishek Modak <amodak828@gmail.com>
2017-08-05 15:35:04 +0530

    color_coding in admin

--- backend/peoples_dashboard/models.py
+++ backend/peoples_dashboard/models.py
@@ -7,7 +7,7 @@ class ColorCoding(models.Model):
     project = models.ForeignKey(
         api_models.Project, db_index=True, related_name='projects_color_codings')
     widget = models.ForeignKey(
-        api_models.Project, db_index=True, related_name='widgets_color_codings')
+        api_models.Widgets, db_index=True, related_name='widgets_color_codings')
     month = models.CharField(max_length=255)
     hard_limit = models.FloatField(null=True, blank=True, default=0.0)
     soft_limit = models.FloatField(null=True, blank=True, default=0.0)
