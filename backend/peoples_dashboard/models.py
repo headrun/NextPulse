6e0d924fe575acec1778a4a3d36e1e315c8f4651
Abhishek Modak <amodak828@gmail.com>
2017-08-05 13:52:10 +0530

    color coding model changes

--- backend/peoples_dashboard/models.py
+++ backend/peoples_dashboard/models.py
@@ -1,5 +1,16 @@
-from __future__ import unicode_literals
 
 from django.db import models
+from api import models as api_models
 
-# Create your models here.
+
+class ColorCoding(models.Model):
+    project = models.ForeignKey(
+        api_models.Project, db_index=True, related_name='projects_color_codings')
+    widget = models.ForeignKey(
+        api_models.Project, db_index=True, related_name='widgets_color_codings')
+    month = models.CharField(max_length=255)
+    hard_limit = models.FloatField(null=True, blank=True, default=0.0)
+    soft_limit = models.FloatField(null=True, blank=True, default=0.0)
+    created_at  = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+
