5aa608cca31fb9bc064ff20759c01949a9df63b3
asifasyed <sdasifa95@gmail.com>
2017-10-31 05:41:14 -0400

    cate week and month view

--- backend/api/voice_widgets.py
+++ backend/api/voice_widgets.py
@@ -221,19 +221,37 @@ def call_status(request):
 
 def cate_dispo_inbound(request):
     result = {}
-    new_date_list = []
+    new_date_list, dates_list, week_names = [], [], []
+    month_names = []
     curr_loca = request.GET['location']
     disposition = request.GET['disposition']
     skill = request.GET['skill']
     main_dict = data_dict(request.GET)
     prj_id = main_dict['pro_cen_mapping'][0][0]
     center = main_dict['pro_cen_mapping'][1][0]
-    dates = main_dict['dwm_dict']['day']
-    date_check = InboundHourlyCall.objects.filter(project = prj_id, center = center, date__range = [dates[0], dates[-1]]).values('date').annotate(total = count('disposition')).order_by('date')
-    values = OrderedDict(zip(map(lambda p: str(p['date']), date_check), map(lambda p: str(p['total']), date_check)))
-    for date_key, date_value in values.iteritems():
-        if date_value > 0:
-            new_date_list.append(date_key)
-    result['cate_dispo_inbound'] = disposition_cate_data(prj_id, center, dates, disposition, curr_loca, skill)
+    if main_dict['dwm_dict'].has_key('day') and main_dict['type'] == 'day':
+        dates = main_dict['dwm_dict']['day']
+        date_check = InboundHourlyCall.objects.filter(project = prj_id, center = center, date__range = [dates[0], dates[-1]]).values('date').annotate(total = count('disposition')).order_by('date')
+        values = OrderedDict(zip(map(lambda p: str(p['date']), date_check), map(lambda p: str(p['total']), date_check)))
+        for date_key, date_value in values.iteritems():
+            if date_value > 0:
+                new_date_list.append(date_key)
+        dispo_cate_val = disposition_cate_data(prj_id, center, dates, disposition, curr_loca, skill)
+        result['cate_dispo_inbound'] = [{'name': item, 'y': dispo_cate_val[item]} for item in dispo_cate_val]
+    elif main_dict['dwm_dict'].has_key('week') and main_dict['type'] == 'week':
+        dates = main_dict['dwm_dict']['week']
+        week_dates = []
+        for date_values in dates:
+            week_dates = week_dates + date_values
+        dispo_cate_val = disposition_cate_data(prj_id, center, week_dates, disposition, curr_loca, skill)
+        result['cate_dispo_inbound'] = [{'name': item, 'y': dispo_cate_val[item]} for item in dispo_cate_val]
+    else:
+        month_dates = []
+        dates = main_dict['dwm_dict']['month']['month_dates']
+        for date_values in dates:
+            month_dates = month_dates + date_values
+        dispo_cate_val = disposition_cate_data(prj_id, center, month_dates, disposition, curr_loca, skill)
+        result['cate_dispo_inbound'] = [{'name': item, 'y': dispo_cate_val[item]} for item in dispo_cate_val]
+    result['type'] = main_dict['type']
     return json_HttpResponse(result)
 
