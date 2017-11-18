3eae4047a04d091dea6288ce3e3db2653f3f128c
abhishekmodak <amodak828@gmail.com>
2017-11-18 15:50:42 +0530

    hourly summary of a selected date range

--- backend/voice_service/voice_widgets.py
+++ backend/voice_service/voice_widgets.py
@@ -1,34 +1,37 @@
-from django.shortcuts import render
-import xlrd
 import datetime
-from django.apps import apps
-from api.models import *
-from api.redis_operations import redis_insert
-from api.basics import *
-from api.uploads import *
-from api.utils import *
-from api.query_generations import *
-from voice_service.voice_query_insertion import *
-from voice_service.constrants import *
-from api.voice_widgets import *
-from xlrd import open_workbook
-from api.commons import data_dict
-from common.utils import getHttpResponse as json_HttpResponse
+from voice_service.models import *
 
 
-def location_data(prj_id, center, dates_list):
-    import pdb;pdb.set_trace()
-    result = {}
-    return result
+def create_filters(filter_params):
+	""" creating filters for voice """
+
+	filter_dict = {}
+	for item in filter_params:
+		for key, value in item.iteritems():
+			if value:
+				_key = '%s__in' %key
+				filter_dict.update(_key:value)
+
+	return filter_dict 
 
 
-def skill_data(prj_id, center, dates_list):
-    result = {}
+def get_hourly_sum(project, dates, table_name, location={}, skill={}, disposition={}):
+	""" take hourly summary of a selected date range """
 
-    return result
+	result_dict = {}
+	filter_param = create_filters([location, skill, disposition, project, dates])
 
-def disposition_data(prj_id, center, dates_list):
-    result = {}
+	for time in xrange(0, 24):
+		result_dict.update({time : 0})
 
-    return result
+	data_set = table_name.objects.filter(**filter_param).values_list('start_time', flat=True)
+	for item in data_set:
+		result_dict[item.hour] +=1
+
+	return result_dict
+
+
+
+
+
 
