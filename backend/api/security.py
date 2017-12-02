eba8d92fe53716c7f5970e1074ed02d233ee6bc8
abhishekmodak <amodak828@gmail.com>
2017-12-02 17:37:58 +0530

    replacing tab to space

--- backend/api/security.py
+++ backend/api/security.py
@@ -3,12 +3,12 @@ from api.review_views import get_all_related_user
 
 
 def get_permitted_user(project, center, user):
-	"""Check whether the user is permitted for the project or not
-	"""
-	var = 'Valid User'
+    """Check whether the user is permitted for the project or not
+    """
+    var = 'Valid User'
     if project and center:
-		user_dict = get_all_related_user(project, center)
-		if user not in user_dict['id_list']:
-			var = 'Invalid User'
+        user_dict = get_all_related_user(project, center)
+        if user not in user_dict['id_list']:
+            var = 'Invalid User'
 
-	return var
+    return var
