eda1a2f4c3334873cdcf66206bbeeeaaebd37306
asifa123 <asifa@head.run>
2018-07-06 15:21:07 +0530

    alignment and fixing issues

--- backend/api/sample_algorithm.py
+++ backend/api/sample_algorithm.py
@@ -18,32 +18,32 @@ def historical_packet_agent_data(request):
 
     ###====return the json response of agents and packets data====###
 
-    result = {}
-    main_data = data_dict(request.GET)
+    result     = {}
+    main_data  = data_dict(request.GET)
     project_id = main_data['pro_cen_mapping'][0][0]
-    center_id = main_data['pro_cen_mapping'][1][0]
-    dates = main_data['dates']
+    center_id  = main_data['pro_cen_mapping'][1][0]
+    dates      = main_data['dates']
 
     if len(dates) > 1:
         raw_query = RawTable.objects.filter(project=project_id,center=center_id,date__range=[dates[0],dates[-1]])
     else:
         raw_query = RawTable.objects.filter(project=project_id,center=center_id,date=dates[0])
-    packets = raw_query.values_list('work_packet',flat=True).distinct()
-    agents = raw_query.values_list('employee_id',flat=True).distinct()
-    prodution = raw_query.aggregate(Sum('per_day'))
+    packets          = raw_query.values_list('work_packet',flat=True).distinct()
+    agents           = raw_query.values_list('employee_id',flat=True).distinct()
+    prodution        = raw_query.aggregate(Sum('per_day'))
     total_production = prodution['per_day__sum']
 
-    packets_result, packet_data = get_the_packet_and_agent_data(packets,dates,project_id,center_id,filter_type='packet')
-    result['config_packets'] = packets_result
-    result['packets'] = packet_data['packets']
-    result['packet_value'] = packet_data['packetvalue']
+    packets_result, packet_data = get_the_packet_and_agent_data(packets,dates,project_id,center_id,filter_type='packets')
+    result['config_packets']    = packets_result
+    result['packets']           = packet_data['packets']
+    result['packet_value']      = packet_data['packetsvalue']
 
-    agents_result, agent_data = get_the_packet_and_agent_data(agents,dates,project_id,center_id,filter_type='agent')
-    result['config_agents'] = agents_result
-    result['agents'] = agent_data['agents']
-    result['agent_value'] = agent_data['agentvalue']
+    agents_result, agent_data   = get_the_packet_and_agent_data(agents,dates,project_id,center_id,filter_type='agents')
+    result['config_agents']     = agents_result
+    result['agents']            = agent_data['agents']
+    result['agent_value']       = agent_data['agentsvalue']
 
-    result['total_production'] = total_production
+    result['total_production']  = total_production
     return JsonResponse(result) 
 
 
@@ -51,38 +51,35 @@ def get_the_packet_and_agent_data(required_data,dates,project_id,center_id,filte
 
     ####===generates the output for displaying top5 packets and agents based on historical data===####
 
-    result_dict = {}
-    result = {}
-
-    values_dict = {}
-
-    data_list = []
+    packet_or_agent_error_data, packet_or_agent_config,packet_or_agent_error_sum = {}, {}, {}
+    
+    data_list   = []
     dates_lists = generate_required_dates(dates, [15,30,60,90])
 
-    values = [1, 0.5, 0.25, 0.125]
+    values      = [1, 0.5, 0.25, 0.125]
     for data in required_data:
         for date_values, factor in zip(dates_lists,values):
-            if filter_type == 'packet':
-                internal_data = Internalerrors.objects.filter(project=project_id,center=center_id,\
+            if filter_type    == 'packets':
+                internal_data  = Internalerrors.objects.filter(project=project_id,center=center_id,\
                     work_packet=data,date__range=[date_values[-1],date_values[0]])
-                external_data = Externalerrors.objects.filter(project=project_id,center=center_id,\
+                external_data  = Externalerrors.objects.filter(project=project_id,center=center_id,\
                     work_packet=data,date__range=[date_values[-1],date_values[0]])
-                raw_data = RawTable.objects.filter(project=project_id,center=center_id,work_packet=data,\
+                raw_data       = RawTable.objects.filter(project=project_id,center=center_id,work_packet=data,\
                     date__range=[date_values[-1],date_values[0]]).aggregate(Sum('per_day'))
-                config_value = Project.objects.get(id=project_id,center=center_id).no_of_packets
+                config_value   = Project.objects.get(id=project_id,center=center_id).no_of_packets
             else:
-                internal_data = Internalerrors.objects.filter(project=project_id,center=center_id,\
+                internal_data  = Internalerrors.objects.filter(project=project_id,center=center_id,\
                     employee_id=data,date__range=[date_values[-1],date_values[0]])
-                external_data = Externalerrors.objects.filter(project=project_id,center=center_id,\
+                external_data  = Externalerrors.objects.filter(project=project_id,center=center_id,\
                     employee_id=data,date__range=[date_values[-1],date_values[0]])
-                raw_data = RawTable.objects.filter(project=project_id,center=center_id,employee_id=data,\
+                raw_data       = RawTable.objects.filter(project=project_id,center=center_id,employee_id=data,\
                     date__range=[date_values[-1],date_values[0]]).aggregate(Sum('per_day'))
-                config_value = Project.objects.get(id=project_id,center=center_id).no_of_agents
+                config_value   = Project.objects.get(id=project_id,center=center_id).no_of_agents
 
-            error_list = [internal_data.aggregate(Sum('total_errors')),external_data.aggregate(Sum('total_errors'))]
-            audit_list = [internal_data.aggregate(Sum('audited_errors')),external_data.aggregate(Sum('audited_errors'))]
+            error_list     = [internal_data.aggregate(Sum('total_errors')),external_data.aggregate(Sum('total_errors'))]
+            audit_list     = [internal_data.aggregate(Sum('audited_errors')),external_data.aggregate(Sum('audited_errors'))]
             
-            total_errors = [ error['total_errors__sum'] if error['total_errors__sum'] is not None else 0 for error in error_list ]
+            total_errors   = [ error['total_errors__sum'] if error['total_errors__sum'] is not None else 0 for error in error_list ]
             audited_errors = [ audit['audited_errors__sum'] if audit['audited_errors__sum'] is not None else 0 for audit in audit_list ]
 
             per_day = 0
@@ -96,32 +93,32 @@ def get_the_packet_and_agent_data(required_data,dates,project_id,center_id,filte
             else:
                 error_value = 0
 
-            if result_dict.has_key(data):
-                result_dict[data].append(round((factor*error_value), 2))
+            if packet_or_agent_error_data.has_key(data):
+                packet_or_agent_error_data[data].append(round((factor*error_value), 2))
             else:
-                result_dict[data] = [round((factor*error_value), 2)]
+                packet_or_agent_error_data[data] = [round((factor*error_value), 2)]
 
-    for key,value in result_dict.iteritems():
-        result[key] = sum(value)
+    for key,value in packet_or_agent_error_data.iteritems():
+        packet_or_agent_error_sum[key] = sum(value)
 
-    sorted_data = (sorted(result.items(), key=itemgetter(1), reverse=True))[:config_value]
-    sorted_names = [re.sub(r'[^\x00-\x7F]+',' ', names[0]) for names in sorted_data]
-    values_dict[filter_type+'value'] = config_value
+    sorted_data                                 = (sorted(packet_or_agent_error_sum.items(), key=itemgetter(1), reverse=True))[:config_value]
+    sorted_names                                = [re.sub(r'[^\x00-\x7F]+',' ', names[0]) for names in sorted_data]
+    packet_or_agent_config[filter_type+'value'] = config_value
     
     for name in required_data:
         if name not in sorted_names:
             data_list.append(name)
-    values_dict[filter_type+'s'] = data_list
-    return sorted_names, values_dict
+    packet_or_agent_config[filter_type] = data_list
+    return sorted_names, packet_or_agent_config
 
 
 def generate_required_dates(dates, required_dates):
 
     ####======generates past 15, 30, 60 and 90 dates based on current database date=====####
 
-    dates = dates[0]
+    dates       = dates[0]
     
-    check_date = date(*map(int, dates.split('-')))
+    check_date  = date(*map(int, dates.split('-')))
     dates_lists = [list(), list(), list(), list()]
     
     for index, date_value in enumerate(required_dates):
@@ -136,92 +133,84 @@ def packet_agent_audit_random(request):
     ###=====claculation of audit%=====###
 
     result = {}
-    data_dict = ast.literal_eval(request.POST.keys()[0])
-
-    audit_value = data_dict.get('audit', "")
-    random_value = data_dict.get('random', "")
-    packets = data_dict.get('packets', "")
-    agents = data_dict.get('agents', "")
-    start_date = data_dict.get('from', "")
-    end_date = data_dict.get('to', "")
-    project = data_dict.get('project', "")
-    center = data_dict.get('center', "").split(' - ')[0]
-    workdone_value = int(data_dict.get('total_production', ""))
-    audited_percentage_value = data_dict.get('audit_value', "")
-    random_percentage_value = data_dict.get('random_value', "")
-    project_id = Project.objects.get(name=project).id
-    center_id = Center.objects.get(name=center).id
+    data_dict         = ast.literal_eval(request.POST.keys()[0])
     
-    audited_percentage_value = int(audited_percentage_value)
-    random_percentage_value = int(random_percentage_value)
-
+    packets           = data_dict.get('packets', "")
+    agents            = data_dict.get('agents', "")
+    start_date        = data_dict.get('from', "")
+    end_date          = data_dict.get('to', "")
+    project           = data_dict.get('project', "")
+    center            = data_dict.get('center', "").split(' - ')[0]
+    workdone_value    = int(data_dict.get('total_production', ""))
+    audited_value     = int(data_dict.get('audit_value', 0))
+    random_value      = int(data_dict.get('random_value', 0))
+    project_id        = Project.objects.get(name=project).id
+    center_id         = Center.objects.get(name=center).id
+    
     k, t, total = 0, 0, 0
     calculated_value = 0
-
-    audit_dict = OrderedDict()
-    random_dict = OrderedDict()
-    common_dict = OrderedDict()
-
+ 
+    audit_dict, random_dict, common_dict = OrderedDict(), OrderedDict(), OrderedDict()
     production_records = RawTable.objects.filter(project=project_id,center=center_id,date=start_date)
 
     for i in xrange(len(production_records)):
-        agent = re.sub(r'[^\x00-\x7F]+',' ', production_records[i].employee_id)
-        work_done = production_records[i].per_day
+        agent       = re.sub(r'[^\x00-\x7F]+',' ', production_records[i].employee_id)
+        work_done   = production_records[i].per_day
         sub_project = production_records[i].sub_project
         work_packet = production_records[i].work_packet
-        sub_packet = production_records[i].sub_packet
+        sub_packet  = production_records[i].sub_packet
 
         data = {"work_done":work_done,"sub_project":sub_project,"work_packet":work_packet,\
                 "sub_packet":sub_packet,"agent":agent,"date":start_date}
-
-        if (calculated_value <= audited_percentage_value) and (audit_value):
+    
+        if (calculated_value <= audited_value):
             if ((work_packet in packets) and (agent in agents)):
                 calculated_value += work_done
-                audit_dict[k] = data
+                audit_dict[k]  = data
                 k += 1
             else:
                 common_dict[t] = data
                 t += 1
         else:
-            common_dict[t] = data
+            common_dict[t]     = data
             t += 1
-    
+      
     random_index = 0
     for index in xrange(len(common_dict)):
         work_packet = common_dict[index]["work_packet"]
-        agent = re.sub(r'[^\x00-\x7F]+',' ', common_dict[index]["agent"])
-        work_done = common_dict[index]["work_done"]
+        agent       = re.sub(r'[^\x00-\x7F]+',' ', common_dict[index]["agent"])
+        work_done   = common_dict[index]["work_done"]
         sub_project = common_dict[index]["sub_project"]
-        sub_packet = common_dict[index]["sub_packet"]
+        sub_packet  = common_dict[index]["sub_packet"]
 
         _data = {"work_done":work_done,"sub_project":sub_project,"work_packet":work_packet,\
                 "sub_packet":sub_packet,"agent":agent,"date":start_date}
-
-        if (calculated_value <= audited_percentage_value) and (audit_value):
+        
+        if (calculated_value <= audited_value):
             if (work_packet in packets) or (agent in agents):
                 calculated_value += work_done
-                audit_dict[k] = _data
+                audit_dict[k]     = _data
                 k += 1
             else:
                 random_dict[random_index] = common_dict[index]
                 random_index += 1
         else:
-            random_dict[random_index] = common_dict[index]
+            random_dict[random_index]     = common_dict[index]
             random_index += 1
 
-    if audited_percentage_value:        
-        if calculated_value >= audited_percentage_value:
+    if audited_value:        
+        if calculated_value >= audited_value:
             result['audit'] = audit_dict
         else:
             result['audit'] = "Please add more Packets and Agents"
 
-    if random_percentage_value:
-        result['random'] = generate_random_data(random_dict,random_percentage_value)
+    if random_value:
+        result['random']    = generate_random_data(random_dict,random_value)
 
     return JsonResponse(result)
 
 
-def generate_random_data(random_dict,random_percentage_value):
+def generate_random_data(random_dict,random_value):
 
     ###==== Random data generation ====###
 
@@ -229,17 +218,18 @@ def generate_random_data(random_dict,random_percentage_value):
 
     _dict = OrderedDict()
     total = 0
-    
-    for index in xrange(len(random_dict)):
-        if total <= random_percentage_value: 
-            value = round(random() * len(random_dict))
-            _dict[index] = random_dict[value]
-            done_value = random_dict[value]["work_done"]
-            total += done_value
+    keys = random_dict.keys()
+    for index in xrange((len(random_dict))-1):
+        if total <= random_value: 
+            value         = int(round(random() * len(random_dict)))
+            if value in keys:
+                _dict[index]  = random_dict[value]
+                done_value    = random_dict[value]["work_done"]
+                total        += done_value
         else:
             break
     
-    if total >= random_percentage_value:
+    if total >= random_value:
         return _dict
     else:
         return "Please Select Low Random Value"
@@ -249,12 +239,12 @@ def generate_excel_for_audit_data(request):
 
     ##=====generates agents and packets data in excel=====##
     if request.method == 'POST':
-        data = ast.literal_eval(request.POST.keys()[0])
-        workbook = xlsxwriter.Workbook('audit_data.xlsx')
-        bold = workbook.add_format({'bold': True})
-        worksheet = workbook.add_worksheet('audit_data')
-        audit_dict = data.get('audit','')
-        random_dict = data.get('random','')
+        data           = ast.literal_eval(request.POST.keys()[0])
+        workbook       = xlsxwriter.Workbook('audit_data.xlsx')
+        bold           = workbook.add_format({'bold': True})
+        worksheet      = workbook.add_worksheet('audit_data')
+        audit_dict     = data.get('audit','')
+        random_dict    = data.get('random','')
         worksheet.write('A'+str(2), 'Date', bold)
         worksheet.write('B'+str(2), 'Emp Name', bold)
         worksheet.write('C'+str(2), 'Sub Project', bold)
