5fdef111f41e16bbd6894e7d3063cdfc6e3508b6
prabha-mnw <prabhakaran.k@mnw.co.in>
2019-01-31 18:17:56 +0530

    Added mobile notif commands

---
+++ backend/api/management/commands/mobile_notification_script.py
@@ -0,0 +1,41 @@
+
+from django.core import mail
+from django.core.mail import send_mail, BadHeaderError
+from django.core.mail import EmailMessage
+from django.db.models import Max, Sum
+from django.core.management.base import BaseCommand, CommandError
+from django.contrib.auth.models import User
+import os
+import datetime
+from itertools import chain
+from api.send_mobile_data import *
+from api.models import *
+from django.db.models import *
+
+
+class Command(BaseCommand):
+
+    commands = ['mobile_sms_report',]
+    args = '[command]'
+    help = 'generating mobile report for production and accuracy to customers and teamleads'
+
+    def handle(self, *args, **options):
+        
+        customers = Customer.objects.filter(name__is_active=True, enable_sms=True)                
+        tls = TeamLead.objects.filter(name__is_active=True,  enable_sms=True)                      
+        
+        for customer in customers:
+            project_list = Customer.objects.filter(id=customer.id, project__is_enable_sms=True, project__is_voice=False, project__display_project=True,).values_list('project', flat=True).distinct()            
+            if len(project_list) > 0:
+                send_mobile_notifications(project_list, customer, 'customer')
+
+        for tl in tls:
+            project_list = TeamLead.objects.filter(id=tl.id, project__is_voice=False, project__display_project=True, project__is_enable_sms=True).values_list('project',flat=True).distinct()                                    
+            if len(project_list) > 0:
+                send_mobile_notifications(project_list, tl, 'team_lead')
+            
+
+        
+
+
+        
